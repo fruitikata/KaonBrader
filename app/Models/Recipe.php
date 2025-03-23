75944c373875973ebf8a2cb07acc107c72ddc984
Kali <liyangnriyang@gmail.com>
2025-03-23 21:03:37 +0800

    Database tables added for Category and Recipe | Transferred imgs to public/

---
+++ app/Models/Recipe.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    /** @use HasFactory<\Database\Factories\RecipeFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'ingredients',
+        'steps',
+        'image',
+        'category_id',  
+    ];
+}
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -19,13 +19,13 @@
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
+            <x-text-input id="name" name="name" type="text" class="block mt-1 w-full border-gray-300 focus:border-yellow-500 focus:ring-yellow-500 dark:focus:border-yellow-600 dark:focus:ring-yellow-600" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
+            <x-text-input id="email" name="email" type="email" class="block mt-1 w-full border-gray-300 focus:border-yellow-500 focus:ring-yellow-500 dark:focus:border-yellow-600 dark:focus:ring-yellow-600" :value="old('email', $user->email)" required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
